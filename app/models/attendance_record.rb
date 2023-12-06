2603f50c87a36c8e4c774b8c65d9db315b15a0ad
King <k19980506@gmail.com>
2023-12-06 20:44:58 +0800

    Fix uniquness bug

--- app/models/attendance_record.rb
+++ app/models/attendance_record.rb
@@ -11,7 +11,7 @@ class AttendanceRecord
   field :event_id, type: String
 
   validates :attendance_status, :user_id, :event_id, presence: true
-  validates :user_id, uniqueness: true, scope: :event_id
+  validates :user_id, uniqueness: { scope: :event_id }
 
   def as_json(_options = {})
     {
