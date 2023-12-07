216ede1a4a4f86ebebb550b96b0d6bbde4588698
King <k19980506@gmail.com>
2023-12-07 19:38:16 +0800

    Delete comment code

--- app/models/attendance_record.rb
+++ app/models/attendance_record.rb
@@ -13,15 +13,6 @@ class AttendanceRecord
   validates :attendance_status, :user_id, :event_id, presence: true
   validates :user_id, uniqueness: { scope: :event_id }
 
-  # def as_json(_options = {})
-  #   {
-  #     id: id.to_s,
-  #     user_id:,
-  #     event_id:,
-  #     attendance_status: formatted_attendance_status
-  #   }
-  # end
-
   private
 
   def formatted_attendance_status
